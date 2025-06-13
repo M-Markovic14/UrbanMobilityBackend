7c2f978437040a6c4cc245c188a6f1977995ca9a
M-Markovic14 <markovicmane04@gmail.com>
2025-06-13 15:38:02 +0200

    Update travellerCRUD.py

--- src/services/travellerCRUD.py
+++ src/services/travellerCRUD.py
@@ -9,13 +9,49 @@ def create_traveller(conn):
     print("=== Register New Traveller ===")
 
     # Get user input with validation
-    first_name = input("First Name: ").strip()
-    last_name = input("Last Name: ").strip()
+    while True:
+        first_name = input("First Name: ").strip()
+        if first_name.isalpha():
+            break
+        else:
+            print("Invalid first name. Only letters allowed")
 
-    birthday = input("Birthday (YYYY-MM-DD): ").strip()
-    gender = input("Gender (male/female): ").strip().lower()
-    street = input("Street: ").strip()
-    house_number = input("House Number: ").strip()
+    while True:
+        last_name = input("Last Name: ").strip()
+        if last_name.isalpha():
+            break
+        else:
+            print("Invalid last name. Only letters allowed")
+
+    while True:
+        birthday = input("Birthday (YYYY-MM-DD): ").strip()
+        try:
+            datetime.strptime(birthday, "%Y-%m-%d")
+            break
+        except ValueError:
+            print("Invalid date. Please use format YYYY-MM-DD.")
+
+
+    while True:
+        gender = input("Gender (male/female): ").strip().lower()
+        if gender in ("male", "female"):
+            break
+        else:
+            print("Invalid input. Please enter 'male' or 'female'.")
+
+    while True:
+        street = input("Street: ").strip()
+        if street.isalpha():
+            break
+        else:
+            print("Invalid street name. Only letters allowed")
+
+    while True:
+        house_number = input("House Number: ").strip()
+        if house_number.isdigit():
+            break
+        else: 
+            print("Invalid House Number. Only numbers allowed")
     
     while True:
         zip_code = input("Zip Code (e.g. 1234AB): ").strip().upper()
@@ -23,8 +59,18 @@ def create_traveller(conn):
             break
         print("Invalid zip code format.")
 
-    city = input("City: ").strip()
-    email = input("Email: ").strip()
+    while True:
+        city = input("city: ").strip()
+        if city.isalpha():
+            break
+        else:
+            print("Invalid city name. Only letters allowed")
+
+    while True:
+        email = input("Email address (e.g. example@mail.com): ").strip().lower()
+        if re.match(r"^[\w\.-]+@[\w\.-]+\.\w{2,}$", email):
+            break
+        print("Invalid email format.")
 
     while True:
         mobile = input("Mobile Phone (e.g. 612345678): ").strip()
@@ -45,8 +91,8 @@ def create_traveller(conn):
         cursor.execute('''
             INSERT INTO travellers (
                 first_name, last_name, birthday, gender,
-                street, house_number, zip_code, city,
-                email, mobile, driving_license, registration_date
+                street_name, house_number, zip_code, city,
+                email, mobile_phone, driving_license, registration_date
             )
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         ''', (
@@ -63,3 +109,5 @@ def create_traveller(conn):
 
     except Exception as e:
         print(f"Error inserting traveller: {e}")
+
+#def 
